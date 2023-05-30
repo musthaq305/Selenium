96b3e126a5a59ce953a877816a42b328c60fe322
musthaq305 <musthaqmohamed305@gmail.com>
2023-05-31 01:56:45 +0530

    Anu done her work

--- src/com/sample/Sample.java
+++ src/com/sample/Sample.java
@@ -2,6 +2,9 @@ package com.sample;
 
 public class Sample {
 
+	private void m1() {
+    System.out.println("Anu code added");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Sample");
