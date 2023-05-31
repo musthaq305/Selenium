c3874b1831a9088840c0e14b9cc680a292d2b067
musthaq305 <musthaqmohamed305@gmail.com>
2023-05-31 13:14:58 +0530

    Sam Added

--- src/com/sample/Sample.java
+++ src/com/sample/Sample.java
@@ -2,6 +2,9 @@ package com.sample;
 
 public class Sample {
 
+	private void m3() {
+    System.out.println("Sam Completed his work");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Sample");
