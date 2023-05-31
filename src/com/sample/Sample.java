78643ab02e8bc014be4ba1e5158e9dfbce343a93
musthaq305 <musthaqmohamed305@gmail.com>
2023-05-31 13:01:02 +0530

    Bala added his code

--- src/com/sample/Sample.java
+++ src/com/sample/Sample.java
@@ -2,6 +2,9 @@ package com.sample;
 
 public class Sample {
 
+	private void m2() {
+    System.out.println("Bala done her Work");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Sample");
